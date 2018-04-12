92da8bff5ecdb6421fd1a49aa7ed1f2a14399f48
nimingzhe7 <38258088+nimingzhe7@users.noreply.github.com>
2018-04-12 15:32:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,5 @@ echo -e -n "$PORT" >> config.json
 echo -e -n "$CONFIG_JSON2" >> config.json
 echo -e -n "$UUID" >> config.json
 echo -e -n "$CONFIG_JSON3" >> config.json
-echo -e -n "$CONFIG_JSON4" >> config.json
-echo -e -n "$CONFIG_JSON5" >> config.json
 
 ./v2ray
